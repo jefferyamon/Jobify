ec824d1bc3fed69025477c446b4b0a9ba0ad0d9e
unknown <jefferyamon2468@gmail.com>
2025-07-20 14:36:39 +0100

    landing page description fix

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -14,12 +14,12 @@ const Landing = () => {
             job <span>tracking</span> app
           </h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate
-            explicabo odio fuga eos aliquid officia iure saepe dicta ex dolore!
-            Iste iusto excepturi mollitia natus neque quod perferendis fuga
-            accusantium est suscipit blanditiis, at enim corrupti autem porro
-            beatae ullam, exercitationem ea, placeat temporibus. Deleniti
-            voluptatum quidem eveniet rerum veniam!
+            This job tracking app streamlines your career management with
+            intuitive features that log applications, deadlines, and interview
+            schedules in one place. It offers customizable status boards,
+            real-time notifications, and analytics to track progress and success
+            rates. Designed for clarity and control, it transforms job hunting
+            into an organized, proactive journey.
           </p>
           <Link to={"/register"} className="btn register-link">
             Register
